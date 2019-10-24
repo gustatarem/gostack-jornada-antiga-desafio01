7b3d4cbb996b4179fdb526f03116f24995a52e0d
Gustavo <gustavo.tatarem@saedigital.com.br>
2019-10-24 00:14:52 -0300

    feat: created get(2) and post(1) methods

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const server = express()
 
 server.use(express.json())
 
+const projects = []
+
+// Método GET que lista todos os projetos e suas tarefas
+
 server.get('/projects', (req, res) => {
-  return res.json({ hello: "world" })
+  return res.json(projects)
+})
+
+// Método POST que recebe um ID e um título vindos do corpo da requisição e os insere no array de projetos
+
+server.post('/projects', (req, res) => {
+  const { id } = req.body
+  const { title } = req.body
+
+  projects.push({id, title})
+
+  return res.json(projects)
 })
 
 server.listen(3000)
